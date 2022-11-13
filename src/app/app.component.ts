d0f017ee8da2746151da233352185ce0adf3a606
soloking2 <soloking2forsure@gmail.com>
2022-11-13 01:12:20 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pwg-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'pwd-generator';
+}
